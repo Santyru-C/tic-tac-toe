d9bbfb3e2440d7cff9d729bb43e5b40cd0ad7aac
Santyru <santiago0988@hotmail.com>
2023-04-05 15:20:03 -0300

    add display functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function GameBoard() {
+const GameBoard = (() => {
   const board = [];
   const rows = 3;
   const columns = 3;
@@ -19,7 +19,7 @@ function GameBoard() {
   };
 
   return { getBoard, modifyCell, cellAvailable };
-}
+})();
 
 function Player(name, markStyle) {
   return { name, markStyle };
@@ -29,22 +29,22 @@ function GameController(name1, name2) {
   const Player1 = Player(name1, 1);
   const Player2 = Player(name2, -1);
   const players = [Player1, Player2];
-  const currentBoard = GameBoard();
 
   let turn = 0;
   let currentPlayerTurn = players[0];
   const gameOver = false;
 
   const getPlayers = () => players;
+  const getCurrentPlayerTurn = () => currentPlayerTurn;
   const switchPlayerTurn = () => {
-    currentPlayerTurn = (currentPlayerTurn === players[0] ? players[1] : players[2]);
+    currentPlayerTurn = (getCurrentPlayerTurn() === players[0] ? players[1] : players[0]);
   };
 
   const checkRows = () => {
     for (let i = 0; i < 3; i++) {
       let rowValue = 0;
       for (let j = 0; j < 3; j++) {
-        rowValue += currentBoard.getBoard()[i][j];
+        rowValue += GameBoard.getBoard()[i][j];
       }
       if (Math.abs(rowValue) === 3) return true;
     }
@@ -54,7 +54,7 @@ function GameController(name1, name2) {
     for (let i = 0; i < 3; i++) {
       let columnValue = 0;
       for (let j = 0; j < 3; j++) {
-        columnValue += currentBoard.getBoard()[j][i];
+        columnValue += GameBoard.getBoard()[j][i];
       }
       if (Math.abs(columnValue) === 3) return true;
     }
@@ -64,11 +64,11 @@ function GameController(name1, name2) {
     let diagonalValue = 0;
     let antidiagonalValue = 0;
     for (let i = 0; i < 3; i++) {
-      diagonalValue += currentBoard.getBoard()[i][i];
+      diagonalValue += GameBoard.getBoard()[i][i];
     }
 
     for (let j = 0; j < 3; j++) {
-      antidiagonalValue += currentBoard.getBoard()[2 - j][j];
+      antidiagonalValue += GameBoard.getBoard()[2 - j][j];
     }
 
     if ((Math.abs(diagonalValue)) === 3 || (Math.abs(antidiagonalValue) === 3)) return true;
@@ -78,24 +78,25 @@ function GameController(name1, name2) {
 
   const checkDraw = () => (!checkWin() && turn === 9);
 
-  const checkGameOver = () => {};
+  const checkGameOver = () => {
+    if (checkWin()) { return 'win'; }
+    if (checkDraw) { return 'draw'; }
+  };
 
   // if game not finished play round
   const PlayRound = (row, column) => {
-    if (currentBoard.cellAvailable(row, column)) {
-      turn += 1;
-
-      currentBoard.modifyCell(row, column, currentPlayerTurn.markStyle);
-      checkGameOver();
-      !gameOver ? switchPlayerTurn() : '';
-    } else {
-      return false;
-    }
+    turn += 1;
+    GameBoard.modifyCell(row, column, getCurrentPlayerTurn().markStyle);
+    console.log(checkGameOver());
+    switchPlayerTurn();
   };
 
-  return { PlayRound, getPlayers };
+  return {
+    PlayRound, getPlayers, switchPlayerTurn, currentPlayerTurn,
+  };
 }
 
+// separate gameboard from player
 const ScreenController = (() => {
   let currentGame;
   const getCurrentGame = () => currentGame;
@@ -104,22 +105,42 @@ const ScreenController = (() => {
     currentGame = GameController(name1, name2);
   }
 
-  const onCellClicked = (cell) => {
+  const addCross = (cell) => {
+    cell.classList.add('x_marked');
+  };
 
+  const addCircle = (cell) => {
+    cell.classList.add('o_marked');
+  };
+
+  const onCellClicked = (cell) => {
+    const { row } = cell.dataset;
+    const { column } = cell.dataset;
+    if (GameBoard.cellAvailable(row, column)) {
+      currentGame.PlayRound(row, column);
+      GameBoard.getBoard()[row][column] === 1 ? addCross(cell) : addCircle(cell);
+    }
   };
 
   const cells = Array.from(document.getElementsByClassName('cell'));
-  cells.forEach((cell) => cell.addEventListener('click', currentGame.PlayRound()));
+  cells.forEach((cell) => cell.addEventListener('click', () => { onCellClicked(cell); }));
 
   return { startGame, getCurrentGame };
 })();
 
+// ---
 function test(e) {
   e.preventDefault();
   const name1 = document.getElementById('player1').value;
   const name2 = document.getElementById('player2').value;
 
   ScreenController.startGame(name1, name2);
+
+  document.getElementById('player1').value = '';
+  document.getElementById('player2').value = '';
+
+  const formWrapper = document.getElementsByClassName('form-wrapper')[0];
+  formWrapper.style.visibility = 'hidden';
 }
 
 const playBtn = document.getElementById('play-btn');
